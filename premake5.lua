5f262ca92eeca919230a609eb6b4a62997fd1b5d
Kushite <kushitejohnson@gmail.com>
2026-06-22 20:16:08 +0300

    Enable x86_64 Premake platform

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,8 @@ local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
 CreateWorkspace({name = "eightbit"})
+	platforms({"x86", "x86_64"})
+
 	CreateProject({serverside = true})
 		IncludeSDKCommon()
 		IncludeSDKTier0()
